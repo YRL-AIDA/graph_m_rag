0ebdc33e2ca9e2541984db8c17866361dd22bd3f
sunveil <mikhailov@icc.ru>
2026-03-24 16:57:06 +0800

    Add LLM client

---
+++ app/src/llm_client.py
@@ -0,0 +1,76 @@
+from openai import OpenAI
+import base64
+from typing import List, Dict, Any, Callable
+import inspect
+
+
+def get_kwargs(kwargs: Dict[str, Any], func: Callable) -> Dict[str, Any]:
+    '''
+        Вытаскивает аргументы из kwargs по сигнатуре функции func.
+    '''
+    sig = inspect.signature(func)
+    return {key: value for key, value in kwargs.items() if key in sig.parameters}
+
+
+class ModelMessageDict(dict):
+    '''
+        Класс - словарь для удобого форматирования запроса к модели.
+        Формирует словарь для передачи в клиента openia и в модель
+    '''
+
+    def __init__(self, role: str = 'user'):
+        super().__init__()
+        self['role'] = role
+        self['content'] = []
+
+    def add_text_content(self, content: str):
+        self['content'].append({'type': 'text',
+                                'text': content})
+
+    def add_img_content(self, source: str = 'image_url', path_to_img: str = None, url: str = None):
+        match source:
+            case 'image_url':
+                if path_to_img != None:
+                    with open(path_to_img, "rb") as f:
+                        base64_image = base64.b64encode(f.read()).decode()
+                    self['content'].append({'type': 'image_url',
+                                            'image_url': {'url': f"data:image/jpeg;base64,{base64_image}"}})
+                elif url != None:
+                    self['content'].append({'type': 'image_url',
+                                            'image_url': {'url': url}})
+
+    def add_img_content_base64(self, base64_image: str = None):
+        self['content'].append({'type': 'image_url', 'image_url': {'url': f"data:image/jpeg;base64,{base64_image}"}})
+
+
+def send_messasge(messages: List[ModelMessageDict], base_url: str = "http://192.168.19.127:8888/v1",
+                  api_key: str = 'EMPTY',
+                  model_name: str = 'Qwen/Qwen3-VL-32B-Thinking', **kwargs) -> List[str]:
+    '''
+        Функция для отправки сообщениий в удаленную модель
+    '''
+    client = OpenAI(api_key=api_key, base_url=base_url, max_retries=1, **get_kwargs(kwargs, OpenAI))
+
+    try:
+        print(f"Generating content with model: {model_name}", )
+
+        response = client.chat.completions.create(
+            messages=messages,
+            model=model_name,
+            # timeout=120,
+            **get_kwargs(kwargs, client.chat.completions.create)
+        )
+
+        return True, [answ.message.content for answ in response.choices]
+
+    except Exception as e:
+        print("Failed to call LLM: " + str(e))
+        if hasattr(e, 'response'):
+            error_info = e.response.json()
+            code_value = error_info['error']['code']
+            print(code_value)
+        else:
+            code_value = "context_length_exceeded"
+            print(code_value)
+        return False, None
+
